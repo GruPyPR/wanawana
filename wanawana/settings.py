712c078fc4ce3cf05a9e5ca68165e4320b4cc5f3
Ellison Leão <ellisonleao@gmail.com>
2016-05-23 20:39:22 -0300

    fix urls and settings

--- wanawana/settings.py
+++ wanawana/settings.py
@@ -45,6 +45,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
